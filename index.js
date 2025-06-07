75d9598392dfe05a065754076d38e94c0114a487
ryanstokes21 <Ryanstokes141@gmail.com>
2025-06-07 13:14:22 -0500

    added a function added random color function to get a random rgb and called it in the createGrid function to get random colors in our squares.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const container = document.querySelector('#container');
 const resizeBtn = document.querySelector('#resize-btn');
 
+
 resizeBtn.addEventListener('click', prompAndCreateGrid);
 function prompAndCreateGrid() {
   let newSize = prompt('Enter new grid size (max 100):');
@@ -14,19 +15,27 @@ function prompAndCreateGrid() {
   }
 };
 
+
 function createGrid(size) {
-container.innerHTML = '';
-for (i = 0; i < size * size; i++) {
-  const square = document.createElement('div');
-  square.classList.add('grid-square');
-  const squareSize = 640 / size;
-  square.style.width = `${squareSize}px`;
-  square.style.height = `${squareSize}px`;
-  square.addEventListener('mousemove', () => {
-    square.style.background = 'black';
-  });
-  container.appendChild(square);
+  container.innerHTML = '';
+  for (i = 0; i < size * size; i++) {
+    const square = document.createElement('div');
+    square.classList.add('grid-square');
+    const squareSize = 640 / size;
+    square.style.width = `${squareSize}px`;
+    square.style.height = `${squareSize}px`;
+    square.addEventListener('mousemove', () => {
+      square.style.background = getRandomColor();
+    });
+    container.appendChild(square);
   };
 };
 
 createGrid(16);
+
+function getRandomColor() {
+  let r = Math.floor(Math.random() * 255)
+  let g = Math.floor(Math.random() * 255)
+  let b = Math.floor(Math.random() * 255)
+  return `rgb(${r}, ${g}, ${b})`
+}
